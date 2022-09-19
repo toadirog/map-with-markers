dd2ad1541001dc2061ef6b52301f3c9a5901723f
toadirog <skobelevgeorge@gmail.com>
2022-09-19 17:58:11 +0300

    layout fixed

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import { Map, Overlay } from 'pigeon-maps';
-import { HeartFilled } from '@ant-design/icons'
+import { HeartFilled } from '@ant-design/icons';
 import { saintPLocation, shegolLocation } from '../constants';
 
 const PigeonMap: FC = () => (
-  <Map height={600} defaultCenter={saintPLocation} defaultZoom={11}>
+  <Map defaultCenter={saintPLocation} defaultZoom={11}>
     <Overlay anchor={shegolLocation}>
-      <HeartFilled style={{color: '#eb2f96', fontSize: '32px'}} />
+      <HeartFilled style={{ color: '#eb2f96', fontSize: '32px' }} />
     </Overlay>
   </Map>
-)
+);
 
 export default PigeonMap;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { Layout } from 'antd';
 import dynamic from 'next/dynamic';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss';
 
+const { Header, Content } = Layout;
 const MapWithNoSSR = dynamic(() => import('../components/Map'), {
   ssr: false,
 });
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div className={styles.header}>
-        <h4>Лера подумала о местах</h4>
-      </div>
-     <MapWithNoSSR />
-    </div>
-  )
-}
+    <Layout>
+      <Header className={styles.header}>Лера подумала о местах</Header>
+      <Content className={styles.content}>
+        <MapWithNoSSR />
+      </Content>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
