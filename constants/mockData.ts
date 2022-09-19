820ab5eea31db9d5dc9b8a36cc0d2dd0cc9d0553
toadirog <skobelevgeorge@gmail.com>
2022-09-19 18:12:27 +0300

    cards sidemenu component added

--- constants/mockData.ts
+++ constants/mockData.ts
@@ -1,4 +1,4 @@
 import { Point } from 'pigeon-maps';
 
 export const shegolLocation: Point = [59.941858, 30.363398]
-export const saintPLocation: Point = [59.939099 , 30.315877]
+export const saintPLocation: Point = [59.939099, 30.315877]
